3bab97ec0c7c436dd0c83dc68ae39281215003a1
Luiz Gustavo <luizgustavo974@hotmail.com>
2026-03-23 13:50:02 -0300

    chore: configure middleware

---
+++ src/lib/supabase/middleware.ts
@@ -0,0 +1,51 @@
+import { createServerClient } from "@supabase/ssr";
+import { NextResponse, type NextRequest } from "next/server";
+
+export async function updateSession(request: NextRequest) {
+  let response = NextResponse.next({
+    request: {
+      headers: request.headers,
+    },
+  });
+
+  const supabase = createServerClient(
+    process.env.NEXT_PUBLIC_SUPABASE_URL!,
+    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
+    {
+      cookies: {
+        getAll() {
+          return request.cookies.getAll();
+        },
+        setAll(cookiesToSet) {
+          cookiesToSet.forEach(({ name, value, options }) =>
+            request.cookies.set(name, value),
+          );
+          response = NextResponse.next({ request });
+          cookiesToSet.forEach(({ name, value, options }) =>
+            response.cookies.set(name, value, options),
+          );
+        },
+      },
+    },
+  );
+
+  const {
+    data: { user },
+  } = await supabase.auth.getUser();
+
+  const url = request.nextUrl.clone();
+  const isLoginPage = url.pathname.startsWith("/login");
+  const isPublicPage = url.pathname.startsWith("/auth") || isLoginPage;
+
+  if (!user && !isPublicPage) {
+    url.pathname = "/login";
+    return NextResponse.redirect(url);
+  }
+
+  if (user && isLoginPage) {
+    url.pathname = "/";
+    return NextResponse.redirect(url);
+  }
+
+  return response;
+}
