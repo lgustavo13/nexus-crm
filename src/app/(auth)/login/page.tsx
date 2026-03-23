e49b1f7922968d1cc0a4dc107b539378f6e95e71
Luiz Gustavo <luizgustavo974@hotmail.com>
2026-03-23 15:03:42 -0300

    feat: create login page

---
+++ src/app/(auth)/login/page.tsx
@@ -0,0 +1,45 @@
+"use client";
+
+import { login } from "@/src/actions/auth-actions";
+import { Button } from "@/src/components/ui/button";
+import { Input } from "@/src/components/ui/input";
+import { Label } from "@/src/components/ui/label";
+import { useActionState } from "react";
+
+export default function LoginPage() {
+  const [state, action, isPending] = useActionState(login, null);
+
+  return (
+    <form action={action} className="space-y-4">
+      <div className="space-y-2">
+        <Label htmlFor="email">Email</Label>
+        <Input
+          id="email"
+          name="email"
+          type="email"
+          placeholder="Digite seu email"
+          required
+        />
+      </div>
+
+      <div className="space-y-2">
+        <Label htmlFor="password">Senha</Label>
+        <Input
+          id="password"
+          name="password"
+          type="password"
+          placeholder="Digite sua senha"
+          required
+        />
+      </div>
+
+      {state?.error && (
+        <p className="text-sm text-red-500 font-medium">{state.error}</p>
+      )}
+
+      <Button type="submit" className="w-full" disabled={isPending}>
+        {isPending ? "Entrando..." : "Entrar"}
+      </Button>
+    </form>
+  );
+}
